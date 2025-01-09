ab2c9a8c076b5d5190c050765319a739a5daa409
Ahemd Goda <83382347+ahmedgoda24@users.noreply.github.com>
2025-01-09 22:25:20 +0200

    fix urls.py

--- project/urls.py
+++ project/urls.py
@@ -22,6 +22,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('file_upload.urls'))
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+] 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
